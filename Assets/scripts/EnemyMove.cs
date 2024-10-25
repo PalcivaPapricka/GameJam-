82c17cfb4839b3d93a75c76d8c2b62fae695ab27
cake814 <79573154+cake814@users.noreply.github.com>
2024-10-25 18:51:21 +0200

    enemy move

---
+++ Assets/scripts/EnemyMove.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyMove
+{
+    public void ZigZagMove()
+    {
+        //spravit mopvement na zigzag
+    }
+
+    public void StraightMove()
+    {
+        //soravit straight movement
+    }
+
+
+}
