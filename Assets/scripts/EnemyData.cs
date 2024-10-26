3498c4cead0e5e3941687de81e355f3e00f18cff
cake814 <79573154+cake814@users.noreply.github.com>
2024-10-26 22:59:40 +0200

    bomber fix

--- Assets/scripts/EnemyData.cs
+++ Assets/scripts/EnemyData.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class EnemyData : ScriptableObject
 {
@@ -9,5 +10,6 @@ public class EnemyData : ScriptableObject
     public float speed;
     public int damage;
     public float detectionRange;
+    public GameObject bullet;
 
 }
---
+++ Assets/scripts/enemyBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBullet : MonoBehaviour
+{
+    private GameObject player;
+
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            player.GetComponent<Clippy>().player_health -= 10;
+        }
+        Destroy(gameObject);
+    }
+}
+
+
